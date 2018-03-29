6670dc69fa5dc38ec187fbd5932277d4d006c9bd
Lisa Lam <lisalam@lisas-mbp-2.fc.ca>
2018-03-29 10:11:17 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,27 +1,60 @@
 (() => {
-  console.log('video stuff loaded!');
+  console.log('video stuff loaded!');
 
-  //variables stack goes here
-  let sigils = document.querySelectorAll('.sigilContainer'),
-  lightbox = document.querySelector('.lightbox'),
-  closeLightBoxButton = lightbox.querySelector('.close-lightbox');
-
-  //functions in the middle!
-  function showHouseVideo() {
-    //debugger;
-    lightbox.classList.add('show-lightbox');
-    //make the video play
-  }
-  function closeLightBox() {
-    //debugger;
-    lightbox.classList.remove('show-lightbox');
-    //stop the video and rewind it to 0
-  }
+  String.prototype.capIt = function() {return this.replace(this.charAt(), this.charAt().toUpperCase()); }
 
 
+  //variables stack goes here
+  let sigils = document.querySelectorAll('.sigilContainer'),
+  lightbox = document.querySelector('.lightbox'),
+  closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
+  vidPlayer = document.querySelector('video'),
+  vidControls = document.querySelector('.controls');
 
-  //event handing at the bottom
-  sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
-  closeLightBoxButton.addEventListener('click', closeLightBox);
+
+  //functions in the middle!
+  function showHouseVideo() {
+    let houseName = this.className.split(' ')[1].capIt();
+    //split apart the class name on the element, grab the house from the result
+    document.querySelector('h1').textContent = `House ${houseName}`;
+    //debugger;
+    lightbox.classList.add('show-lightbox');
+    //make the video play
+    vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`
+    vidPlayer.load();
+    vidPlayer.play();
+
+  }
+
+
+  function closeLightBox() {
+    //debugger;
+    lightbox.classList.remove('show-lightbox');
+    //stop the video and rewind it to 0
+    vidPlayer.pause();
+    vidPlayer.currentTime = 0;
+  }
+
+function pausePlay() {
+  //debugger;
+  let theButton = this.firstElementChild;
+  //
+  if (vidPlayer.paused == true) {
+    //play the video
+    vidPlayer.play();
+    theButton.dataset.icon = "pause-circle";
+  } else{
+    vidPlayer.pause();
+    theButton.dataset.icon = 'play-circle';
+  }
+
+}
+
+
+  //event handing at the bottom
+  sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
+  closeLightBoxButton.addEventListener('click', closeLightBox);
+  vidPlayer.addEventListener('ended', closeLightbox);
+  vidControls.addEventListener('click', pausePlay);
 
 })();
