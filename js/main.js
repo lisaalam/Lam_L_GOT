da72dc6f62ade78c7b923bfca20465c363bde23c
Lisa Lam <lisalam@Lisas-MacBook-Pro-2.local>
2018-04-11 11:27:42 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -5,56 +5,65 @@
 
 
   //variables stack goes here
-  let sigils = document.querySelectorAll('.sigilContainer'),
-  lightbox = document.querySelector('.lightbox'),
-  closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
-  vidPlayer = document.querySelector('video'),
-  vidControls = document.querySelector('.controls');
+let sigils = document.querySelectorAll('.sigilContainer'),
+    lightbox = document.querySelector('.lightbox'),
+    closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
+    vidPlayer = document.querySelector('video'),
+    vidControls = document.querySelector('.selector'),
+    imageBanner = document.querySelector('#houseImages');
 
 
   //functions in the middle!
   function showHouseVideo() {
     let houseName = this.className.split(' ')[1].capIt();
-    //split apart the class name on the element, grab the house from the result
-    document.querySelector('h1').textContent = `House ${houseName}`;
+    //split apart the class name on the element, grab the
+    document.querySelector('h1').textContent = `House ${houseName}`;
     //debugger;
-    lightbox.classList.add('show-lightbox');
+    //lightbox.classList.add('show-lightbox');
     //make the video play
-    vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`
+    vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
     vidPlayer.load();
-    vidPlayer.play();
+    vidPlayer.play();
+
+    scrollBanners(this.dataset.offset)
 
   }
 
+  function scrollBanners(offset) {
+    // move the banner images to the left
+    let moveIt = offset * 600 + "px";
+
+    imageBanner.style.right = moveIt;
+  }
 
   function closeLightBox() {
     //debugger;
     lightbox.classList.remove('show-lightbox');
     //stop the video and rewind it to 0
-    vidPlayer.pause();
-    vidPlayer.currentTime = 0;
+  vidPlayer.pause();
+  vidPlayer.currentTime = 0;
+
   }
 
-function pausePlay() {
-  //debugger;
-  let theButton = this.firstElementChild;
-  //
-  if (vidPlayer.paused == true) {
-    //play the video
-    vidPlayer.play();
-    theButton.dataset.icon = "pause-circle";
-  } else{
-    vidPlayer.pause();
-    theButton.dataset.icon = 'play-circle';
-  }
+  function pausePlay() {
+    //debugger
+    let theButton = this.firstElementChild;
+
+    if (vidPlayer.paused == true) {
+      //play the
+      vidPlayer.play();
+      theButton.dataset.icon = "pause-circle";
+    } else {
+    vidPlayer.pause();
+    theButton.dataset.icon = "play-circle";
+  }
 
 }
-
 
   //event handing at the bottom
   sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
   closeLightBoxButton.addEventListener('click', closeLightBox);
-  vidPlayer.addEventListener('ended', closeLightbox);
+  vidPlayer.addEventListener('ended', closeLightbox);
   vidControls.addEventListener('click', pausePlay);
 
 })();
