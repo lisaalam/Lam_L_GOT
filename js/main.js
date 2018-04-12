9151a62d027cdfbae518598f3bcda870e893c0c2
Lisa Lam <lisalam@lisas-mbp-2.fc.ca>
2018-04-12 11:18:14 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -9,8 +9,13 @@ let sigils = document.querySelectorAll('.sigilContainer'),
     lightbox = document.querySelector('.lightbox'),
     closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
     vidPlayer = document.querySelector('video'),
-    vidControls = document.querySelector('.selector'),
+    vidControls = document.querySelector('.controls'),
     imageBanner = document.querySelector('#houseImages');
+
+    function scrollBanners(offset) {
+        let moveIt = offset * 600 + "px";
+          imageBanner.style.right = moveIt;
+    }
 
 
   //functions in the middle!
@@ -19,13 +24,13 @@ let sigils = document.querySelectorAll('.sigilContainer'),
     //split apart the class name on the element, grab the
     document.querySelector('h1').textContent = `House ${houseName}`;
     //debugger;
-    //lightbox.classList.add('show-lightbox');
+    lightbox.classList.add('show-lightbox');
     //make the video play
     vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
     vidPlayer.load();
     vidPlayer.play();
 
-    scrollBanners(this.dataset.offset)
+    scrollBanners(this.dataset.offset);
 
   }
 
